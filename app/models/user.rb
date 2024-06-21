3271e34af8d739197c295accd9f3c702a3497a97
piyofisu <koyapiro.2011.20@gmail.com>
2024-06-21 18:35:36 +0900

    バリデーション設定2

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX
   validates :name, presence: true
-  validates :nickname
+  validates :nickname, presence: true
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
     validates :last_name
     validates :first_name
