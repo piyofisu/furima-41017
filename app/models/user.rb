7b3ede36ab4410bc68ce8c95c3d4cab53a4681af
piyofisu <koyapiro.2011.20@gmail.com>
2024-06-21 21:56:34 +0900

    エラーメッセージの重複修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX
+  validates_format_of :password, with: PASSWORD_REGEX, allow_blank: true
   validates :nickname, presence: true
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+  with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
     validates :last_name
     validates :first_name
   end
-  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/ } do
+  with_options format: { with: /\A[ァ-ヶ]+\z/ } do
     validates :last_name_katakana
     validates :first_name_katakana
   end
