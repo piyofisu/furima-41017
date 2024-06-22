0170572a083283a3e2cf86e0850763b57b3839c5
piyofisu <koyapiro.2011.20@gmail.com>
2024-06-22 22:16:20 +0900

    Rubocop自動修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
   validates_format_of :password, with: PASSWORD_REGEX, allow_blank: true
   validates :nickname, presence: true
   with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    email                 {Faker::Internet.email}
-    password              {"4G" + Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
-    nickname              {Faker::Name.initials(number: 2)}
-    last_name             {"松ザか"}
-    first_name            {"大すケ"}
-    last_name_katakana    {"マツザカ"}
-    first_name_katakana   {"ダイスケ"}
-    birth_day             {Faker::Date.birthday(min_age: 5, max_age: 94)}
+    email                 { Faker::Internet.email }
+    password              { '4G' + Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    nickname              { Faker::Name.initials(number: 2) }
+    last_name             { '松ザか' }
+    first_name            { '大すケ' }
+    last_name_katakana    { 'マツザカ' }
+    first_name_katakana   { 'ダイスケ' }
+    birth_day             { Faker::Date.birthday(min_age: 5, max_age: 94) }
   end
 end
