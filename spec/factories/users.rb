87bb2cbc387a6848e39f52881648becadb8828e5
piyofisu <koyapiro.2011.20@gmail.com>
2024-06-22 20:50:19 +0900

    Userモデルテスト・正常系

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    email                 {Faker::Internet.email}
+    password              {"4G" + Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    nickname              {Faker::Name.initials(number: 2)}
+    last_name             {"松ザか"}
+    first_name            {"大すケ"}
+    last_name_katakana    {"マツザカ"}
+    first_name_katakana   {"ダイスケ"}
+    birth_day             {Faker::Date.birthday}
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    context '新規登録できる場合'
+      it 'email、password、password_confirmation、nickname、last_name、first_name
+      、last_name_katakana、first_name_katakana、birth_dayが存在すれば登録できる' do
+        expect(@user).to be_valid
+      end
+    end
+    # context '新規登録できない場合' do
+      # it ''
 end
