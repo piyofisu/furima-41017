f3e76a86c7550c17ad50700595ce030bf0e2edf9
piyofisu <koyapiro.2011.20@gmail.com>
2024-06-24 17:05:14 +0900

    商品情報保存のストロングパラメーター

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,14 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+  end
+
+  private
+  
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :condition_id, 
+                                :charge_id, :prefecture_id, :until_shipping_id,
+                                :price).merge(user_id: currnet_user.id)
 end
