366ba436c1f6b48d560209153e53e31887cb2307
piyofisu <koyapiro.2011.20@gmail.com>
2024-06-24 17:09:39 +0900

    ActiveStorage・画像加工ツールの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,6 +9,7 @@ class ItemsController < ApplicationController
   
   def item_params
     params.require(:item).permit(:name, :description, :category_id, :condition_id, 
-                                :charge_id, :prefecture_id, :until_shipping_id,
-                                :price).merge(user_id: currnet_user.id)
+     :charge_id, :prefecture_id, :until_shipping_id, :price, :image
+     ).merge(user_id: currnet_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,11 @@
 class Item < ApplicationRecord
+  has_one_attached :image
+  
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
   belongs_to :charge
   belongs_to :prefecture
   belongs_to :until_shipping
+
 end
