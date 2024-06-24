b70c042cdecf590372ec3b3b35eb766e75e90ad2
piyofisu <koyapiro.2011.20@gmail.com>
2024-06-24 19:09:58 +0900

    Itemモデル・バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   has_one_attached :image
-  
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
@@ -8,4 +8,15 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :until_shipping
 
+  validates :name, :description, presence: true
+  validates :description, :category_id, :condition_id, :charge_id,
+    :prefecture_id, :until_shipping_id, numericality: { 
+    other_than: 1 , message: "cant't be blank"
+    }
+  validates :price, numericality: { 
+    only_integer: true,
+    greater_than_or_equal_to: 300,
+    less_than_or_equal_to: 9999999 
+    }, 
+    format: { with: /\A[0-9]+\z/ }
 end
