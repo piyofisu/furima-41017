ffeb053a0287a1d5e671c600b69f44e560732b94
piyofisu <koyapiro.2011.20@gmail.com>
2024-06-26 15:58:24 +0900

    Rubocop自動修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,10 +14,9 @@ class Item < ApplicationRecord
             :until_shipping_id, numericality: {
               other_than: 1, message: "cant't be blank"
             }
-  validates :price, numericality: {only_integer: true,
-  greater_than_or_equal_to: 300,
-  less_than_or_equal_to: 9999999,
-  message: 'is invalid'
-  }
+  validates :price, numericality: { only_integer: true,
+                                    greater_than_or_equal_to: 300,
+                                    less_than_or_equal_to: 9_999_999,
+                                    message: 'is invalid' }
   # validates :price, format: { with: /\A([3-9][0-9]{2,}|[1-9][0-9]{3,6})\z/,  }
 end
