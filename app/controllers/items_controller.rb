e51627c5f8ba6ac6362d9fd4aaa06e64a7393276
piyofisu <koyapiro.2011.20@gmail.com>
2024-06-26 21:34:55 +0900

    商品削除機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
   before_action :set_items, only: [:show, :edit, :update]
 
   def index
@@ -36,6 +36,12 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to root_path
+  end
+  
   private
 
   def item_params
