997681c763625f6e60dd0bb1c42127d97283cb5d
piyofisu <koyapiro.2011.20@gmail.com>
2024-07-02 22:20:16 +0900

    修正：バリデーションの記述の改善

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -5,13 +5,12 @@ class PurchaseAddress
                 :token
 
   with_options presence: true do
-    validates :user_id, :item_id, :municipalities, :street_address
+    validates :user_id, :item_id, :municipalities, :street_address, :token
     validates :post, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)', allow_blank: true }
     validates :phone_number,
               format: { with: /\A\d{10,11}\z/, message: 'must be a number between 10 and 11 digits.', allow_blank: true }
   end
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :token, presence: true
 
   def save
     purchase = Purchase.create(user_id:, item_id:)
